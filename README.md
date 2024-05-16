d600871c6ebab84d6fc57fe769e9f3bb958c22c4
Clément Robert <cr52@protonmail.com>
2024-05-16 14:43:01 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Illustrative reproducer for ...
+
+How to reproduce
+```shell
+$(shell) run.sh
+```
+
+then open `htmlcov/index.html` and compare with terminal outputs.
--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 set -euxo pipefail
 
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install -r requirements.txt
+
 python -m coverage run -m pytest
 python -m coverage report
 python -m coverage html
